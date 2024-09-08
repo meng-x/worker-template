d04a2fc674b8ccf2d30cdfa4c0f1fb30f63d11f8
Joe M <3647619+meng-x@users.noreply.github.com>
2024-09-08 21:59:16 +0900

    Update handler.py

--- src/handler.py
+++ src/handler.py
@@ -10,9 +10,24 @@ def handler(job):
     """ Handler function that will be used to process jobs. """
     job_input = job['input']
 
-    name = job_input.get('name', 'World')
+    dance_id = job_input.get('dance_id')
+    img_url = job_input.get('img_url')
 
-    return f"Hello, {name}!"
+    // prepare dance vidoe 
+
+    // download image
+
+    // process inference
+    copy-paste
+
+    process_id = subProcess(f'python3.11 inference.py ..{param1}.{param1}.{param1}.'
+
+    judge process_id
+    // upload generated video (firebase)
+
+    // get uploaded video url
+
+    return {url: url}
 
 
 runpod.serverless.start({"handler": handler})
